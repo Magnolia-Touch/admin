516feb044d53ebe3ae3873590ba178f8fa18ac31
cuddleminddev <cuddleminddev@gmail.com>
2025-08-13 14:39:20 +0530

    header dropdown

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,15 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { NavigationEnd, Router, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, CommonModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
 export class LayoutComponent {
   pageTitle = '';
   isSidebarOpen = false;
+  isProfileMenuOpen = false;
 
   constructor(private router: Router) {
     this.router.events.subscribe(event => {
@@ -28,5 +30,23 @@ export class LayoutComponent {
     if (window.innerWidth <= 768) {
       this.isSidebarOpen = false;
     }
+  }
+
+  toggleProfileMenu() {
+    this.isProfileMenuOpen = !this.isProfileMenuOpen;
+  }
+
+  closeProfileMenu() {
+    this.isProfileMenuOpen = false;
+  }
+
+  goToProfile() {
+    this.router.navigate(['/profile']);
+    this.isProfileMenuOpen = false;
+  }
+
+  logout() {
+    console.log("Logged out");
+    this.isProfileMenuOpen = false;
   }
 }
